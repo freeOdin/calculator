8cde72e756784f0f555ae69f591fe93a7c28de9d
freeOdin <159131665+freeOdin@users.noreply.github.com>
2024-10-21 13:18:11 +0100

    Add operate function to perform calculations

--- javascript.js
+++ javascript.js
@@ -2,3 +2,19 @@ const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
+
+let num1;
+let num2;
+let operator;
+
+const operate = (num1, operator, num2) => {
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return sub(num1, num2);
+  } else if (operator == "*") {
+    return mul(num1, num2);
+  } else if (operator == "/") {
+    return div(num1, num2);
+  }
+}
